9137af36cc083436d6d1a36fe612197af656d188
Anthony Shaga <antonyshaga@gmail.com>
2025-04-19 04:48:04 +0300

    Обновление проекта новым лендингом

--- src/app/i18n/get-dictionary.ts
+++ src/app/i18n/get-dictionary.ts
@@ -1,9 +1,10 @@
 import 'server-only'
+import {Dictionary} from "@/types/dictionary";
 
 const dictionaries = {
     en: () => import('../[lang]/dictionaries/ en.json').then((module) => module.default),
     ru: () => import('../[lang]/dictionaries/ru.json').then((module) => module.default),
 }
 
-export const getDictionary = async (locale: 'en' | 'ru') =>
+export const getDictionary = async (locale: 'en' | 'ru') :Promise<Dictionary> =>
     dictionaries[locale]?.() ?? dictionaries.en()
