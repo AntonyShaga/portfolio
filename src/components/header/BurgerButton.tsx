7fa43ad6a235b624783dc5af2b3ca3b39361c9c0
Anthony Shaga <antonyshaga@gmail.com>
2025-05-01 05:46:21 +0300

    fix: BurgerMenu

--- src/components/header/BurgerButton.tsx
+++ src/components/header/BurgerButton.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import { motion, AnimatePresence } from 'framer-motion';
+import {AnimatePresence} from 'framer-motion';
 import Cross from "@/icons/Cross";
 import Burger from "@/icons/Burger";
 import Button from "@/components/ui/Button";
+import MotionWrapper from "@/components/ui/MotionWrapper";
 
 interface BurgerButtonProps {
     open: boolean;
@@ -15,7 +16,7 @@ export default function BurgerButton({ open, onClickAction }: BurgerButtonProps)
         <Button onClick={onClickAction} aria-label="Toggle menu">
             <AnimatePresence initial={false} mode="wait">
                 {open ? (
-                    <motion.div
+                    <MotionWrapper
                         key="cross"
                         initial={{ opacity: 0, rotate: -180 }}
                         animate={{ opacity: 1, rotate: 0 }}
@@ -23,9 +24,9 @@ export default function BurgerButton({ open, onClickAction }: BurgerButtonProps)
                         transition={{ duration: 0.3, ease: "easeInOut" }}
                     >
                         <Cross className="w-6 h-6" />
-                    </motion.div>
+                    </MotionWrapper>
                 ) : (
-                    <motion.div
+                    <MotionWrapper
                         key="burger"
                         initial={{ opacity: 0, rotate: 180 }}
                         animate={{ opacity: 1, rotate: 0 }}
@@ -33,7 +34,7 @@ export default function BurgerButton({ open, onClickAction }: BurgerButtonProps)
                         transition={{ duration: 0.3, ease: "easeInOut" }}
                     >
                         <Burger className="w-6 h-6" />
-                    </motion.div>
+                    </MotionWrapper>
                 )}
             </AnimatePresence>
         </Button>
